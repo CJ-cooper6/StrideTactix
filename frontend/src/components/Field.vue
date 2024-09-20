9c3d690263a05c9685437d7c6d0474cc79a479f0
cooper <473172339@qq.com>
2024-09-20 17:35:19 +0800

    更新ui

--- frontend/src/components/Field.vue
+++ frontend/src/components/Field.vue
@@ -5,15 +5,24 @@
      
       preserveAspectRatio="xMidYMid meet"
     >
-     <!-- 背景一 -->
-     <rect width="1300" height="900" fill="var(--primary-brand-color-lightened-5)" />
+    <!-- 背景一 -->
+    <rect width="1280" height="880" fill="none" />
      <!-- 背景二 -->
-     <rect width="1170" height="800" transform="translate(65, 50)" fill="var(--primary-brand-color-lightened-3)" />
+     <rect width="1150" height="780" transform="translate(65, 50)" fill="var(--primary-brand-color-lightened-2)" />
     
      <!-- 球场 -->
-     <g transform="translate(125, 120)">
+     <g transform="translate(115, 100)">
       <!-- 场地元素 -->
-      <rect x="0" y="0" width="1050" height="680" fill="var(--primary-brand-color-lightened-3)" />
+      <rect x="0" y="0" width="1050" height="680" fill="var(--primary-color)" />
+      <!-- 草坪纹理 -->
+      <path d="M0 0 H87.5 V680 H0 V0
+        M175 0 H262.5 V680 H175 V0
+        M350 0 H437.5 V680 H350 V0
+        M525 0 H612.5 V680 H525 V0
+        M700 0 H787.5 V680 H700 V0
+        M875 0 H962.5 V680 H875 V0
+        M1050 0 H1050 V680 H1050 V0" 
+        fill="#72A653" stroke="none" stroke-linecap="square" />
       <!-- 白线 -->
       <rect x="0" y="0" width="1050" height="680" fill="none" stroke="white" stroke-width="2" />
       <!-- 中线 -->
@@ -44,13 +53,13 @@
     </g>
 
     <!-- 操作按钮 -->
-    <g id="control-buttons" transform="translate(75, 10)"@click="clear">
+    <g id="control-buttons" transform="translate(65, 10)"@click="clear">
       <rect width="100" height="40" rx="5" fill="#2196F3"/>
-      <text x="50" y="25" text-anchor="middle" fill="white" dominant-baseline="middle">全部清除</text>
+      <text x="50" y="25" text-anchor="middle" fill="white" dominant-baseline="middle" class="no-select">全部清除</text>
     </g>
 
     <!-- 工具面板 -->
-    <g id="tools-panel" transform="translate(65, 850)">
+    <g id="tools-panel" transform="translate(75, 830)">
       <ItemComponent
       v-for="item in toolItems"
         :item="item"
@@ -126,5 +135,9 @@ const clear = () => {
 #field {
   transition: all 0.5s ease;
   touch-action: none; /* 防止浏览器默认的触摸行为 */
+
+  .no-select {
+    user-select: none;
+  }
 }
 </style>
